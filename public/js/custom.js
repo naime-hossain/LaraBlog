d24f1aca147690e490a105f333b307d2f1bcb865
naime <hossain.naime@yahoo.com>
2017-07-05 17:39:46 +0600

    minor

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,3 +1,11 @@
+  
+  $(window).on('load', function() {
+
+             $('#status').fadeOut(); // will first fade out the loading animation
+            $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+            $('body').delay(350).css({'overflow':'visible'});
+      
+        });
   $(document).ready(function() { // makes sure the whole site is loaded
          
 
@@ -7,10 +15,4 @@
         });
         });   
 
-      $(window).ready(
-        function() {  
-             $('#status').fadeOut(); // will first fade out the loading animation
-            $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
-            $('body').delay(350).css({'overflow':'visible'});
-      
-        });
+   
