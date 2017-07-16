537d3281e4eba70d0c2787676f291d759e41a005
Saiful Islam <saaifulislam@gmail.com>
2017-07-16 13:13:38 +0600

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,13 @@ const { mix } = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css/style.css');
+mix.sass('resources/assets/sass/app.scss', 'public/css/style.css');
+
+
+mix.combine([
+    "node_modules/jquery/dist/jquery.js", 
+    "resources/assets//js/jquery.nicescroll.min.js", 
+    "resources/assets//js/material.min.js", 
+    "resources/assets//js/material-kit.js", 
+    "resources/assets//js/material-kit.js",
+], 'public/assets/js/app.js');
