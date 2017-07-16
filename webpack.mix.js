79c05b94b281a2aa6a4e007ce7f452f2c974d2d7
naime <hossain.naime@yahoo.com>
2017-07-16 15:22:24 +0600

    remove vue and add all js in one file using .combine

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,21 @@ const { mix } = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css/style.css');
+// mix.js('resources/assets/js/app.js', 'public/js')
+//    .sass('resources/assets/sass/app.scss', 'public/css/style.css');
+ mix.sass('resources/assets/sass/app.scss', 'public/css/style.css');
+ 
+
+ mix.combine([
+     "node_modules/jquery/dist/jquery.js", 
+     "node_modules/bootstrap/dist/js/bootstrap.min.js", 
+    "public/js/jquery.nicescroll.min.js", 
+    "public/js/material.min.js", 
+     "public/js/material-kit.js", 
+     "public/js/custom.js", 
+     ], 
+    'public/assets/js/app.js'
+
+
+ );
+ // mix.combine(['one.js', 'two.js'], 'merged.js');
